48dc1fbf018af50d0e839f29d00476cd71457424
DragosAnca <anca.dragos.00@gmail.com>
2024-05-14 20:53:43 +0300

    Added Swagger to main branch

--- ArticlesApi/Program.cs
+++ ArticlesApi/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddSingleton<IArticlesRepository, ArticlesRepository>();
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,6 +30,21 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// Use Swagger and Swagger UI
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Articles API V1");
+    c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+});
+
+// Redirect root URL to Swagger UI
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/swagger");
+    return Task.CompletedTask;
+});
 
 //app.UseHttpsRedirection();
 app.UseStaticFiles();
