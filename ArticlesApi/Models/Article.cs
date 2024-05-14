4009a59d41099511fd89295ab0a37eeb914c8e0c
DragosAnca <anca.dragos.00@gmail.com>
2024-05-14 15:14:52 +0300

    Add project files.

---
+++ ArticlesApi/Models/Article.cs
@@ -0,0 +1,10 @@
+﻿namespace ArticlesApi.Models
+{
+    public class Article
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content {  get; set; }
+        public DateTime PublishDate {  get; set; }
+    }
+}
